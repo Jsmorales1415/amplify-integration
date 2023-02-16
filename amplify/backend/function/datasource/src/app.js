f2885d91309263963a9869d112886c950d750287
Jsmorales1415 <jsmorales1415@gmail.com>
2023-02-16 15:30:56 -0500

    Auth imported and function added

---
+++ amplify/backend/function/datasource/src/app.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware');
+const AWS = require('aws-sdk');
+
+let passwordSecret;
+
+const connectionString = async () => {
+  const ssm = new AWS.SSM({ region: process.env.REGION });
+  const parameter = await ssm
+    .getParameter({
+      Name: process.env.RDS_PASSWORD,
+      WithDecryption: true,
+    })
+    .promise();
+  return parameter.Parameter.Value;
+};
+
+var mysql = require('mysql');
+let connection;
+
+// var connection = mysql.createConnection({
+//   database: process.env.RDS_DATABASE,
+//   host: process.env.RDS_HOSTNAME,
+//   user: process.env.RDS_USERNAME,
+//   password: passwordSecret,
+//   port: process.env.RDS_PORT,
+// });
+
+(async () => {
+  const cs = await connectionString();
+  connection = mysql.createConnection(JSON.parse(cs));
+
+  connection.connect(function (err) {
+    if (err) {
+      console.error('Database connection failed: ' + err.stack);
+      return;
+    }
+    console.log('Connected to database.');
+  });
+})();
+
+// declare a new express app
+const app = express();
+app.use(bodyParser.json());
+app.use(awsServerlessExpressMiddleware.eventContext());
+
+// Enable CORS for all methods
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+  next();
+});
+
+app.get('/items', function (req, res) {
+  const data = connection.query(
+    'SELECT * FROM activity',
+    function (err, result, fields) {
+      if (err) {
+        res.status(400).json({ error: 'Query failed!' });
+        throw err;
+      }
+      res.json(result);
+    }
+  );
+  console.log({ data });
+});
+
+app.listen(3000, function () {
+  console.log('App started');
+});
+
+module.exports = app;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Auth } from 'aws-amplify';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import '../../configureAmplify';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
+
+// export default withAuthenticator(App, { variation: 'modal' });
+export default App;
