d3c2db929b1514631109c535b4c0a28e37207301
Sebastian Cordero Ramirez <43121242+scordero414@users.noreply.github.com>
2023-02-28 15:25:54 -0500

    Feat/auth (#1)
    * amplify pull
    
    * Implement auth by aws amplify
    
    ---------
    
    Co-authored-by: scordero414 <scordero414@Sebastians-MacBook-Pro-2.local>

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,41 +1,12 @@
 export type AmplifyDependentResourcesAttributes = {
   "api": {
-    "AdminQueries": {
-      "ApiId": "string",
-      "ApiName": "string",
-      "RootUrl": "string"
-    },
     "api536f5774": {
       "ApiId": "string",
       "ApiName": "string",
       "RootUrl": "string"
     }
   },
-  "auth": {
-    "amplifyintegration112db461112db461": {
-      "AppClientID": "string",
-      "AppClientIDWeb": "string",
-      "CreatedSNSRole": "string",
-      "IdentityPoolId": "string",
-      "IdentityPoolName": "string",
-      "UserPoolArn": "string",
-      "UserPoolId": "string",
-      "UserPoolName": "string"
-    },
-    "userPoolGroups": {
-      "adminGroupRole": "string",
-      "mobileGroupRole": "string",
-      "superadminGroupRole": "string"
-    }
-  },
   "function": {
-    "AdminQueriesa85aa44c": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
-    },
     "datasource": {
       "Arn": "string",
       "LambdaExecutionRole": "string",
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,113 +1,37 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import logo from '../../public/next.svg';
-import { Inter } from '@next/font/google';
-import styles from '@/styles/Home.module.css';
-import { Auth } from 'aws-amplify';
-import { useState } from 'react';
+import { useAuthenticator } from '@aws-amplify/ui-react';
+import { Button, Grid, Typography } from '@mui/material';
+import { useRouter } from 'next/router';
+function Home() {
+  const router = useRouter();
 
-const inter = Inter({ subsets: ['latin'] });
+  const { user, signOut } = useAuthenticator((context) => [context.user]);
 
-const NOTSIGNIN = 'You are NOT logged in';
-const SIGNEDIN = 'You have logged in successfully';
-const SIGNEDOUT = 'You have logged out successfully';
-const WAITINGFOROTP = 'Enter OTP number';
-const VERIFYNUMBER = 'Verifying number (Country code +XX needed)';
-
-export default function Home() {
-  const [message, setMessage] = useState('Welcome to Demo');
-  const [number, setNumber] = useState('');
-  const [session, setSession] = useState(null);
-  const [user, setUser] = useState(null);
-  const [otp, setOtp] = useState('');
-
-  const verifyAuth = () => {
-    Auth.currentAuthenticatedUser()
-      .then((user) => {
-        setUser(user);
-        setMessage(SIGNEDIN);
-        setSession(null);
-      })
-      .catch((err) => {
-        console.error(err);
-        setMessage(NOTSIGNIN);
-      });
+  const onLogOut = () => {
+    signOut();
   };
-
-  const signIn = () => {
-    setMessage(VERIFYNUMBER);
-    Auth.signIn(number)
-      .then((result) => {
-        setSession(result);
-        setMessage(WAITINGFOROTP);
-      })
-      .catch((e) => {
-        if (e.code === 'UserNotFoundException') {
-          console.log('User not found');
-        } else if (e.code === 'UsernameExistsException') {
-          setMessage(WAITINGFOROTP);
-          signIn();
-        } else {
-          console.log(e.code);
-          console.error(e);
-        }
-      });
-  };
-
-  const signOut = () => {
-    if (user) {
-      Auth.signOut();
-      setUser(null);
-      setOtp('');
-      setMessage(SIGNEDOUT);
-    } else {
-      setMessage(NOTSIGNIN);
-    }
-  };
-
-  const verifyOtp = () => {
-    Auth.sendCustomChallengeAnswer(session, otp)
-      .then((user) => {
-        setUser(user);
-        setMessage(SIGNEDIN);
-        setSession(null);
-      })
-      .catch((err) => {
-        setMessage(err.message);
-        setOtp('');
-        console.log(err);
-      });
-  };
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{message}</p>
-        {!user && !session && (
-          <div>
-            <input
-              placeholder="Phone Number (+XX)"
-              onChange={(event) => setNumber(event.target.value)}
-            />
-            <button onClick={signIn}>Get OTP</button>
-          </div>
-        )}
-        {!user && session && (
-          <div>
-            <input
-              placeholder="Your OTP"
-              onChange={(event) => setOtp(event.target.value)}
-              value={otp}
-            />
-            <button onClick={verifyOtp}>Confirm </button>
-          </div>
-        )}
-        <div>
-          <button onClick={verifyAuth}>Am I sign in?</button>
-          <button onClick={signOut}>Sign Out</button>
-        </div>
-      </header>
-    </div>
+    <Grid
+      container
+      sx={{ height: '100vh', justifyContent: 'center', alignItems: 'center' }}
+    >
+      <Grid item xs={10} md={4}>
+        <Typography textAlign='center' variant='h2'>
+          HOME
+        </Typography>
+        <Typography textAlign='center' variant='h4'>
+          {user?.username}
+        </Typography>
+        <Button
+          fullWidth
+          sx={{ backgroundColor: '#77C115' }}
+          variant='contained'
+          onClick={onLogOut}
+        >
+          Log Out
+        </Button>
+      </Grid>
+    </Grid>
   );
 }
+
+export default Home;
